52df09bf89e5bf87b699bf2dce8af38eb73f0b89
Gabriel L <gabriellovato15@gmail.com>
2022-06-23 09:44:36 -0300

    Adjusted header, made progress on Logo and Profile components

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,8 +1,14 @@
 
 import styles from "./style.module.scss";
 
+import dropDownSVG from "/src/assets/drop-down.svg";
+
 export function Profile() {
   return (
-    <img src="https://github.com/gabrielLV-BR.png" alt="" />
+    <div className={styles.profile}>
+      <img className={styles.profilePicture} src="https://github.com/gabrielLV-BR.png" alt="" />
+      <p>Your Name</p>  
+      <img className={styles.dropdown} src={dropDownSVG} alt="" />
+    </div>
   );
 }
