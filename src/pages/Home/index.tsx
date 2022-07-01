1ad4c07514d0b0070027e00c0e2f64958913a7bc
Gabriel L <gabriellovato15@gmail.com>
2022-07-01 20:42:59 -0300

    Created Home with random placed dots, might remove later but looks kinda cool now

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,44 @@
-import React from "react"
-import { ReactDOM } from "react"
-
-import styles from "style.module.scss";
+import styles from "./style.module.scss";
+import { Button } from "../../components/Button";
+import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 
 export function Home() {
+
+  let balls : {x: number, y: number, r: number}[] = [];
+
+  for(let i = 0; i < 4; i++) {
+    const r = 150 + Math.random() * 300;
+    const x = Math.random() * innerWidth - r / 2;
+    const y = Math.random() * innerHeight - r / 2;
+
+    balls = [...balls, {x,y,r}];
+  }
+
   return (
     <>
       <Header/>
-      <h1>Hello</h1>
+      <main className={styles.main}>
+        <h1>Escutar muda<br />tudo</h1>
+        <p>Milhões de músicas e podcasts para baixar e escutar</p>
+        <Button>Baixe o Spotify de graça</Button>
+        {
+          balls.map(ball => 
+            <div 
+              className={styles.ball} 
+              style={{
+                left: ball.x, 
+                top: ball.y, 
+                width: ball.r, 
+                height: ball.r,
+                opacity: (0.2 + Math.random()) * 0.7
+              }}
+
+            ></div>  
+          )
+        }
+      </main>
+      <Footer/>
     </>
   )
 }
